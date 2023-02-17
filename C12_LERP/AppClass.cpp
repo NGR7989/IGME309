b605082edc4339ad4eba710572e444b5b0981acd
Narai Risser <89745834+NGR7989@users.noreply.github.com>
2023-02-17 14:18:51 -0500

    Did lerping?????

--- C12_LERP/AppClass.cpp
+++ C12_LERP/AppClass.cpp
@@ -63,47 +63,55 @@ void Application::Display(void)
 	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
 
 	//Calculate list of Stops
-	static std::vector<vector3> v3Stop;
+	static std::vector<vector3> points;
 	static bool bInit = false; //Initialize once
 	if (!bInit)
 	{
-		v3Stop.push_back(vector3(-3, -0, 0));
-		v3Stop.push_back(vector3(3, -0, 0));
-		v3Stop.push_back(vector3(0, 2.5, 0));
-		v3Stop.push_back(vector3(0, -2.5, 0));
+
+		points.push_back(vector3(-4.0f, -2.0f, 5.0f));
+		points.push_back(vector3(1.0f, -2.0f, 5.0f));
+		points.push_back(vector3(-3.0f, -1.0f, 3.0f));
+		points.push_back(vector3(2.0f, -1.0f, 3.0f));
+		points.push_back(vector3(-2.0f, 0.0f, 0.0f));
+		points.push_back(vector3(3.0f, 0.0f, 0.0f));
+		points.push_back(vector3(-1.0f, 1.0f, -3.0f));
+		points.push_back(vector3(4.0f, 1.0f, -3.0f));
+		points.push_back(vector3(0.0f, 2.0f, -5.0f));
+		points.push_back(vector3(5.0f, 2.0f, -5.0f));
+		points.push_back(vector3(1.0f, 3.0f, -5.0f));
 		bInit = true;
 	}
 
 	vector3 v3Start; //start point
 	vector3 v3End; //end point
 	static uint route = 0; //current route
-	v3Start = v3Stop[route]; //start at the current route
-	v3End = v3Stop[(route + 1) % v3Stop.size()]; //end at route +1 (if overboard will restart from 0)
+	v3Start = points[route]; //start at the current route
+	v3End = points[(route + 1) % points.size()]; //end at route +1 (if overboard will restart from 0)
 
 	//get the percentace
 	float fTimeBetweenStops = 2.0;//in seconds
 	//map the value to be between 0.0 and 1.0
-	float fPercentage = MapValue(fTimer, 0.0f, fTimeBetweenStops, 0.0f, 1.0f);
+	float lerp = MapValue(fTimer, 0.0f, fTimeBetweenStops, 0.0f, 1.0f);
 
 	//calculate the current position
-	vector3 v3CurrentPos = glm::lerp(v3Start, v3End, fPercentage);
+	vector3 v3CurrentPos = glm::lerp(v3Start, v3End, lerp);
 	matrix4 m4Model = glm::translate(IDENTITY_M4, v3CurrentPos);
 
 	//if we are done with this route
-	if (fPercentage >= 1.0f)
+	if (lerp >= 1.0f)
 	{
 		route++; //go to the next route
 		fTimer = m_pSystem->GetDeltaTime(uClock);//restart the clock
-		route %= v3Stop.size();//make sure we are within boundries
+		route %= points.size();//make sure we are within boundries
 	}
 
 	// render the object
 	m_pMesh->Render(m4Projection, m4View, m4Model);
 
 	// draw stops to know we are within stops
-	for (uint i = 0; i < v3Stop.size(); ++i)
+	for (uint i = 0; i < points.size(); ++i)
 	{
-		m_pModelMngr->AddSphereToRenderList(glm::translate(v3Stop[i]) * glm::scale(vector3(0.15f)), C_RED, RENDER_WIRE);
+		m_pModelMngr->AddSphereToRenderList(glm::translate(points[i]) * glm::scale(vector3(0.15f)), C_RED, RENDER_WIRE);
 	}
 
 	// draw a skybox
